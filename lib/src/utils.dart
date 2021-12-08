007f48d7d37c95a6f0d07d18395a516ee6434440
Himanshu Arora <himanshu.arora@procol.in>
2021-12-08 12:56:10 +0530

    change utils function

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,10 +2,6 @@ import 'dart:math';
 
 class Utils {
   static double getValueUptoSpecificDecimals(double value, int precision) {
-    double newValue = value * double.parse(pow(10, precision).toString());
-    int ceilValue = newValue.ceil();
-    double finalValue =
-        ceilValue.toDouble() / double.parse(pow(10, precision).toString());
-    return finalValue;
+    return double.tryParse(value.toStringAsFixed(precision))!;
   }
 }
